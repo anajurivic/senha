b8f93f1dfac9d1c704efeaf3bf9de121b876a28e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 21:37:53 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿const string SenhaCorreta = "1234abcd";
+string senha, permissaoAcesso;
+
+
+
+Console.Write("Olá, usuário. Por favor, digite sua senha: ");
+senha = Console.ReadLine()!;
+
+
+
+permissaoAcesso = senha == SenhaCorreta ? "permitido" : "negado";
+
+
+
+Console.WriteLine($"Acesso {permissaoAcesso}");
